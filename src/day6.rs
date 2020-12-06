766d32f51bc233c780f6489be9f2c7915f4a1a83
Adam <grzybojad@gmail.com>
2020-12-06 15:03:35 +0100

    Day 6 complete

--- src/day6.rs
+++ src/day6.rs
@@ -1,19 +1,46 @@
 ﻿#[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> Vec<String> {
+pub fn input_generator(input: &str) -> Vec<Vec<String>> {
     input
         .split("\n\n" )
         .map(|group| {
-            let mut answers: Vec<char> = group.chars().filter(|c| c.is_alphabetic()).collect();
-            answers.sort();
-            answers.dedup();
-            answers.into_iter().collect()
+            let single = group.split("\n");
+            
+            single.map(|s| {
+                let mut answers: Vec<char> = s.chars().filter(|c| c.is_alphabetic()).collect();
+                answers.sort();
+                answers.dedup();
+                answers.into_iter().collect()
+            }).collect()
+            
         }).collect()
 }
 
 #[aoc(day6, part1)]
-pub fn solve_part1(input: &[String]) -> usize {
+pub fn solve_part1(input: &[Vec<String>]) -> usize {
     input
         .into_iter()
-        .map(|l| l.len())
+        .map(|group| {
+            let mut ans: Vec<char> = group.join("").chars().collect();
+            ans.sort();
+            ans.dedup();
+            ans.len()
+        }).sum()
+}
+
+#[aoc(day6, part2)]
+pub fn solve_part2(input: &[Vec<String>]) -> usize {
+    input
+        .into_iter()
+        .map(|group| {
+            let common: String = 
+                group[0]
+                    .chars()
+                    .filter(|k| 
+                        group[1..].iter().all(|s| 
+                            (*s).contains(*k))
+                    ).collect();
+            
+            common.len()
+        })
         .sum()
 }
