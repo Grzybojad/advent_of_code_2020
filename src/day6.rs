596ac035f8d51ad633af577703211bbdbe7f2e51
Adam <grzybojad@gmail.com>
2020-12-06 10:58:22 +0100

    Day 6 part 1 solved

---
+++ src/day6.rs
@@ -0,0 +1,19 @@
+﻿#[aoc_generator(day6)]
+pub fn input_generator(input: &str) -> Vec<String> {
+    input
+        .split("\n\n" )
+        .map(|group| {
+            let mut answers: Vec<char> = group.chars().filter(|c| c.is_alphabetic()).collect();
+            answers.sort();
+            answers.dedup();
+            answers.into_iter().collect()
+        }).collect()
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &[String]) -> usize {
+    input
+        .into_iter()
+        .map(|l| l.len())
+        .sum()
+}
