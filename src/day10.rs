3309e1add42076eb481941641080d4d78d98c4ee
Adam <grzybojad@gmail.com>
2020-12-10 19:59:42 +0100

    Day 10 complete

--- src/day10.rs
+++ src/day10.rs
@@ -30,3 +30,44 @@ pub fn solve_part1(input: &[i32]) -> i32 {
     
     one_jump * (three_jump+1)
 }
+
+#[aoc(day10, part2)]
+pub fn solve_part2(input: &[i32]) -> usize {
+    let mut prev_jolt = 0;
+    let mut input_iter = input.into_iter();
+
+    let mut paths: usize = 1;
+    let mut single_jolt_count = 0;
+    while let Some(jolt) = input_iter.next() {
+        match jolt - prev_jolt {
+            3 => {
+                if single_jolt_count > 1 {
+                    paths *= arrangements(single_jolt_count-1);
+                }
+                single_jolt_count = 0;
+            },
+            1 => single_jolt_count += 1,
+            _ => panic!(),
+        }
+
+        prev_jolt = *jolt;
+    }
+    
+    // Add the device itself
+    if single_jolt_count > 1 {
+        paths *= arrangements(single_jolt_count-1);
+    }
+
+    paths
+}
+
+fn arrangements(segment_len: i32) -> usize {
+    match segment_len {
+        1 => 2,
+        2 => 4,
+        3 => 7,
+        4 => 14,
+        5 => 24,
+        _ => panic!(),
+    }
+}
